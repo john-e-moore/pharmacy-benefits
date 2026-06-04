67e3449bfb4c4f077d57488c349721ccadce4a9e
john-e-moore <jmoore87jr@gmail.com>
2026-06-04 16:15:50 -0400

    relaunch docs after running pipeline

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")/.."
 
 rm -f data/raw/.pipeline_timestamp
 
+# generate data
 python scripts/generate_synthetic_plans.py
 python scripts/generate_synthetic_drugs.py
 python scripts/generate_synthetic_pharmacies.py
@@ -15,5 +16,10 @@ python scripts/generate_synthetic_members.py
 python scripts/generate_synthetic_claims.py
 python scripts/load_raw_to_snowflake.py
 
+# build dev
 dbt source freshness
 dbt build --target dev
+
+# refresh docs
+dbt compile --write-index --write-catalog # new with dbt fusion; used to be dbt docs generate
+dbt docs serve --no-open --host 0.0.0.0 --port 8580 # necessary for WSL2
